aa82578f782fe1a4af21e9b9089f30e2d7800eaf
Bala Luguja <bluguja@gmail.com>
2020-05-09 17:02:50 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in firstProject-cli.gemspec
+gemspec
---
+++ lib/firstProject/cli.rb
@@ -0,0 +1,8 @@
+require "firstProject/cli/version"
+
+module FirstProject
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
