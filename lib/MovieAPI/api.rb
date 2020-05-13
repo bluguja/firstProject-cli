e709c23e6596f7d6236e14edaa980b5724bc0641
Bala Luguja <bluguja@gmail.com>
2020-05-13 09:31:47 +0000

    made ap.rb changes

--- lib/MovieAPI/api.rb
+++ lib/MovieAPI/api.rb
@@ -4,7 +4,13 @@ class MovieAPI::API
       url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=#{key}" 
       
       response = HTTParty.get(url) 
-      binding.pry 
+     # binding.pry 
+     # @ 32.31 minutes
+     name = movie["display_title"]
+     rating = movie["mppa_rating"] 
+     link = movie["link"] ["url"]
+     # new instance of a Movie created
+     MovieAPI::Movie.new(name, rating,link)
       
     end 
     
