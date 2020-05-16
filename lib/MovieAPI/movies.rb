baea2eeaf7ef1f0a2b915355855ae8a54e51c56c
Bala Luguja <bluguja@gmail.com>
2020-05-16 08:13:39 +0000

    updaate movie class

--- lib/MovieAPI/movies.rb
+++ lib/MovieAPI/movies.rb
@@ -17,20 +17,5 @@ class Movies
   def self.all 
     @@all 
   end 
-end 
-#binding.pry
-
-### in these methods we'll define how the information on each object can be returned
-
-#this method returns movie names eg "the God father"
-def movie_name
-end
-
-
-# this method returns movie ratings eg R rated, G rated et al
-def movie_rating 
-end 
-
-# this method returns the review link
-def movie_ling
-end
+  
+ 
