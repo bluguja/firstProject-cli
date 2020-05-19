fd414ecf2bb1127428ca2367b26e7cc590d5bc3e
Bala Luguja <bluguja@gmail.com>
2020-05-19 10:14:02 +0000

    new changes to bin

--- lib/MovieAPI/api.rb
+++ lib/MovieAPI/api.rb
@@ -6,11 +6,11 @@ class API
     def self.fetch_web_info
       
       key = ENV["MY_API_KEY"]
-      binding.pry
+      
      
       url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=#{key}"
       
-      response = HTTParty.get(url) 
+      response = HTTParty.get(URI(url))
      binding.pry
      # @ 32.31 minutes
      
