4d5fb02d12954682b993d2fe54da8b05500867b5
Bala Luguja <bluguja@gmail.com>
2020-05-20 17:02:58 +0000

    new changes cli

--- lib/MovieAPI/cli.rb
+++ lib/MovieAPI/cli.rb
@@ -2,7 +2,10 @@ class CLI
   # thiss file is used to interface with the user
   # executes the flow of what the application does
   
-  def run
+  def run 
+    
+    input = ""
+    while input != "exit"
     
     puts" "
     puts "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo".green
@@ -15,15 +18,28 @@ class CLI
     puts"oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo".green
     puts " "
     puts " "
-    puts" Enter the title of the movie  to see the rating of the movie, the link "
-    puts" to the reviews,and a short summary of the tiled movie "
-    puts " "
+    puts" To exit at any time type 'exit'"
+    puts" To see the titles of movies  we have enter 'title'"
+    puts" To see the link to the movies we have enter 'link'"
+    puts" To see the MPPA ratings we have enter 'rating' "
     
-    # input strips any spaces and lines , an downcase ensures all characters entered are low case
-    @get_input_from_user = gets.strip.downcase 
+    input = ""
+    while input != "exit"
+    #input strips any spaces and lines , an downcase ensures all characters entered are low case
+    input = gets.strip.downcase 
     
      API.fetch_web_info # :: shows its nested in the MOVIEAPI folder
+     case input
+     when "title"
+       list_titles
+     when "link"
+       list_links
+     when "rating"
+       list rating
+         
+      
    end
+ end
    
    
      def call_movie 
