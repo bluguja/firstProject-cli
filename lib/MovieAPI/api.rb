5ffab489731c6b59f177a9cea55f471585857358
Bala Luguja <bluguja@gmail.com>
2020-05-21 20:28:32 +0000

    made changes to cli, api files

--- lib/MovieAPI/api.rb
+++ lib/MovieAPI/api.rb
@@ -3,17 +3,17 @@
 class API
   
     
-    def self.fetch_web_info
+    def self.fetch_web_info(web_info)
       
       key = ENV["MY_API_KEY"]
       
      
-      url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=#{key}"
+      url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=#{web_info}&api-key=#{key}"
       
       response = HTTParty.get(URI(url))
-      
+    
       response["results"].each do |movie|
-        
+        #binding.pry
         name = movie["display_title"]
         rating = movie["mpaa_rating"]
         link = movie["link"]["url"]
@@ -21,7 +21,7 @@ class API
         Movie.new(name,rating,link)# new movie instatnce takes in a name, a rating , and a link 34sexit
         
         
-      binding.pry
+      #binding.pry
      end 
     end
   end 
--- lib/MovieAPI/cli.rb
+++ lib/MovieAPI/cli.rb
@@ -5,7 +5,7 @@ class CLI
   def run 
     
     input = ""
-    
+     
     if input == "exit" || input == ""
       puts "Thanks have a nice day!"
     end
@@ -24,58 +24,52 @@ class CLI
     puts " "
     puts " "
     puts" To exit at any time type 'exit'"
-    puts" To see the titles of movies  we have enter 'title'"
-    puts" To see the link to the movies we have enter 'link'"
-    puts" To see the MPPA ratings we have enter 'rating' "
+    puts" To see the titles of movie, the ratings and the link To"
+    puts "the critics web, enter the  title of movie"
     puts " "
+    
     
     input = ""
     while input != "exit"
     #input strips any spaces and lines , an downcase ensures all characters entered are low case
     input = gets.strip.downcase 
     
-     API.fetch_web_info # :: shows its nested in the MOVIEAPI folder
-     
-     case input
-     when "title"
-       list_titles
-     when "link"
-       list_links
-     when "rating"
-       list_ratings
-         
-    end  
-   end
+     API.fetch_web_info(input) # :: shows its nested in the MOVIEAPI folder
+     CLI.list_titles
+     binding.pry
+     end
   end
  end
    
    
-     def call_movie 
-       API.new.fetch_web_info   # fetches the info from the url
+     def self.call_movie 
+       API.new.fetch_web_info()   # fetches the info from the url
        
        Movie.all.each do |movie|# iterates through the Movies class in the @@all array
      end
      
-      def list_titles
+    end
+     
+      def self.list_titles
         Movie.all.sort_by do |display_title|
-          display_title.name
+          puts display_title.name
         end
       end 
       
-      def list_links
+      def self.list_links
          Movie.all.sort_by do |display_link|
-          display_link.name
+         puts display_link.name
         end
       end 
       
-      def list_ratings 
+      def self.list_ratings 
          Movie.all.sort_by do |display_rating|
-          display_rating.name 
+          puts display_rating.name 
         end
       end
        
        #binding.pry
-  end
+  
 end
    
 
