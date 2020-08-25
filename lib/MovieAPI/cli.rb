d8fa7a1bcfd5739f51c38ae8458a03ee6c9546c2
Bala Luguja <bluguja@gmail.com>
2020-08-25 18:54:02 +0000

    supdates

--- lib/MovieAPI/cli.rb
+++ lib/MovieAPI/cli.rb
@@ -76,8 +76,10 @@
       start
       end
     end
+    
+    # create option for user to see a numbered list of the movies that exist in the @@all array.
+    # when the user picks a movie from that numbered list, we don't make another api call, but rather we look at our object class (Movie) and find the object Movie object that matches the user's selection , and lists the details of that Movie object once again for the user.
 
-       
        def self.call_movie 
        
        API.new.fetch_web_info  # fetches the info from the url
